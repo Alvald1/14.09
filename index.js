dd22721bf267f302190ca29c08528ea315099beb
Afanasyy <gri.kirill2005@yandex.ru>
2021-09-18 13:51:11 +0300

    added random pos

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 var last_id = 1;
 function myFoo() {
-  var div = document.getElementById("con");
-  var btn_f = document.getElementById("btn" + last_id);
-  var btn = btn_f.cloneNode(true);
-  btn.id = "btn" + ++last_id;
+  let div = document.getElementById("main");
+  let con_f = document.getElementById("con" + last_id);
+  let btn_f = con_f.children[0];
+  let con = con_f.cloneNode(true);
+  con.id = "con" + ++last_id;
+  let btn = con.children[0];
   btn_f.setAttribute("disabled", true);
   btn.style.backgroundColor = randColor();
-  btn.style.left = "800px";
-  div.appendChild(btn);
+  div.appendChild(con);
+  randPos();
 }
 function randColor() {
-  var r = Math.floor(Math.random() * 256),
+  let r = Math.floor(Math.random() * 256),
     g = Math.floor(Math.random() * 256),
     b = Math.floor(Math.random() * 256);
   return "#" + r.toString(16) + g.toString(16) + b.toString(16);
+}
+
+function randPos() {
+  const box = document.getElementById("con" + last_id);
+  let btn = box.children[0];
+  const height = window.innerHeight - btn.style.height;
+  const width = window.innerWidth - btn.style.width;
+  let randY = Math.floor(Math.random() * height + 1);
+  let randX = Math.floor(Math.random() * width + 1);
+  box.style.transform = `translate(${randX}px, ${randY}px)`;
 }
